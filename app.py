07144733128a8578cd6197046aa7034281a8a4d1
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2023-10-29 19:16:25 +0530

    removed api key

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_social_media_analytics(handle):
     app.sign_in(username=username, password=password)
     usertweets= app.get_tweets(username=handle,pages=1)
     a=('\n').join([tweet.text for tweet in usertweets.tweets])
-    openai.api_key = "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"
+    openai.api_key = ""
 
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
